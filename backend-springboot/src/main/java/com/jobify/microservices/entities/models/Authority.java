f499a191bec77620da9cc180719aa58874e7d054
Rishabh Singh <rishabh@ask-fast.com>
2022-01-06 15:54:38 +0530

    removed frontend folder

--- backend-springboot/src/main/java/com/jobify/microservices/entities/models/Authority.java
+++ backend-springboot/src/main/java/com/jobify/microservices/entities/models/Authority.java
@@ -8,4 +8,3 @@ public class Authority implements GrantedAuthority {
         return null;
     }
 }
-cd e:
--- backend-springboot/src/main/java/com/jobify/microservices/utilities/StaticConstantsUtil.java
+++ backend-springboot/src/main/java/com/jobify/microservices/utilities/StaticConstantsUtil.java
@@ -8,7 +8,7 @@ public class StaticConstantsUtil {
     public static final String NOT_NULL = " can not be null";
 
     //jwt util properties
-    public static final String ISSUER = "rishabhSingh18";
+    public static final String ISSUER = "rishthakur18@gmail.com";
     public static final int ACCESS_TOKEN_VALIDITY_SECONDS = 600;
     public static final byte[] JWT_SECRET_KEY = "rishi1234".getBytes();
 
